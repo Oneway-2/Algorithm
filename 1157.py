08e2a854b443df7028cc61acf73c08fbdd0a8238
Kim Hankil <nuclear0789@naver.com>
2021-05-18 04:57:41 +0900

    1157

---
+++ 1157.py
@@ -0,0 +1,13 @@
+word = list(input().upper())
+spell_count = [0] * 26
+
+for j in range(len(word)):
+    for i in range(26):
+        if chr(i+65) == word[j]:
+            spell_count[i] += 1
+            continue
+
+if spell_count.count(max(spell_count)) >= 2: 
+    print("?")
+else:
+    print(chr(spell_count.index(max(spell_count))+65))
