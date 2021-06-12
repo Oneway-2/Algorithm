7991d30c66d28d6d240c6ad017cd1d14686fd6bb
Kim Hankil <nuclear0789@naver.com>
2021-06-12 23:42:27 +0900

    practice

--- practice.py
+++ practice.py
@@ -1,6 +1,8 @@
-def a():
-    return 3
-def b():
-    return 5
+alist = []
+for _ in range(8):
+    number = int(input())
+    alist.append(number)
+num_max = max(alist)
+print(num_max)
+print(alist.index(num_max)+1)
 
-print(a()+b())
