c056ca456f610ee47fc0c175be47f3d8b7fa0289
Kim Hankil <nuclear0789@naver.com>
2021-06-25 21:25:06 +0900

    practice

--- practice.py
+++ practice.py
@@ -1,4 +1,6 @@
-import sys
-T = int(input())
-nlist = list(map(int, sys.stdin.readline().split()))
-print(nlist)
+alist = ['wont','wait','more']
+blist = ['a','b','c','d','e','f','g','h','i']
+alist.sort()
+for i in range(len(alist)):
+    blist[i+2] = alist[i]
+print(blist)
