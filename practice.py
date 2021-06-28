d0ab843a2036b6e6f161a5d91dcffd8ba7c6facc
Kim Hankil <nuclear0789@naver.com>
2021-06-28 19:46:55 +0900

    practice

--- practice.py
+++ practice.py
@@ -1,6 +1,1 @@
-alist = ['wont','wait','more']
-blist = ['a','b','c','d','e','f','g','h','i']
-alist.sort()
-for i in range(len(alist)):
-    blist[i+2] = alist[i]
-print(blist)
+print(pow(10, 10, 10))
