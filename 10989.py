1fe370d0b99d63e46cb93ddc4803d28d462477e1
Kim Hankil <nuclear0789@naver.com>
2021-06-30 02:01:10 +0900

    20210630 10989 직사각형에서 탈출

---
+++ 10989.py
@@ -0,0 +1,18 @@
+# sorting numbers 3
+
+
+
+
+
+
+
+# import sys
+# n = int(input())
+# nlist = []
+# for i in range(n):
+#     a = int(sys.stdin.readline())
+#     nlist.append(a)
+# nlist.sort()
+# for p in nlist:
+#     print(p)
+# 2750번 문제랑 완전 똑같길래 불안했긴한데 역시 메모리초과 남
