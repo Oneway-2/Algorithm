dbac4e460033d30b57a36f1efcc8a6e3c159f148
Kim Hankil <nuclear0789@naver.com>
2021-07-03 17:48:04 +0900

    practice

--- practice.py
+++ practice.py
@@ -1,7 +1,14 @@
-dicts = {0:1, 1:1, 2:2, 3:3, 4:4, 5:5, 6:5, 7:6, 8:7, 9:8, 10:9}
-print(dicts)
+# dicts = {0:1, 1:1, 2:2, 3:3, 4:4, 5:5, 6:5, 7:6, 8:7, 9:8, 10:9}
+# print(dicts)
+# print(dicts[6])
+# dicts[6] += 1
+# print(dicts[6])
+# dicts_k = list(dicts.keys())
+# dicts_v = list(dicts.values())
 
-for i in range(len(dicts)):
-    for key, value in dicts.items():
-        if value == i:
-            print("i == {}, key == {}".format(i, key))
+# print(dicts_k)
+# print(dicts_v)
+# print(dicts_v.index(5))\
+
+nlist = [1,1,1,1,1,2,3,3,3,3,3]
+print(nlist.index(3,4))
