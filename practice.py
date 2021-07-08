afda49315ecf9f029e140dfaf4ef45d68e7059e7
Kim Hankil <nuclear0789@naver.com>
2021-07-08 23:51:48 +0900

    practice

--- practice.py
+++ practice.py
@@ -1,14 +1,1 @@
-# n = 2
-# a = "abcdef012346"
-
-# for i in range(0, n*6, 6):
-#     print("{}".format(i))
-#     print(a[i:i+6])
-
-# print(a[0:6])
-# print(a[6:12])
-
-a = 3
-print(type(3))
-if type(a) == type(1):
-    print("yes")
+print(0 or 3)
