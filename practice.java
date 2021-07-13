2311c093bae669afbee3159734df0f7977685bcc
Kim Hankil <nuclear0789@naver.com>
2021-07-13 23:58:48 +0900

    practice

--- practice.java
+++ practice.java
@@ -2,6 +2,6 @@ import jdk.javadoc.internal.doclets.formats.html.resources.standard;
 
 public class practice {
     public static void main(String[] args) {
-        System.out.println("Install Successed?");
+        System.out.println("Install Successed??");
     }
 }
