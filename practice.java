892c7f0ecd606c39496a27d57bc52ff2092247f2
Kim Hankil <nuclear0789@naver.com>
2021-07-16 00:00:52 +0900

    delete java

--- practice.java
+++
@@ -1,7 +0,0 @@
-import jdk.javadoc.internal.doclets.formats.html.resources.standard;
-
-public class practice {
-    public static void main(String[] args) {
-        System.out.println("Install Successed??");
-    }
-}
