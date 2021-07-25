167315a17c0da819e2f5844b2901b8151f32b948
Kim Hankil <nuclear0789@naver.com>
2021-07-25 22:38:20 +0900

    practice

--- Java/src/algorithm/Practice.java
+++ Java/src/algorithm/Practice.java
@@ -1,7 +1,13 @@
 package algorithm;
 
+import java.util.Arrays;
+
 public class Practice {
 	public static void main(String[] args) {
-		System.out.println("working?");
+		int[] a = new int[] {1,2,3,4,5,6};
+		int[] b = new int[10];
+		System.out.println(Arrays.toString(b));
+		b = Arrays.copyOfRange(a, 2, 4);
+		System.out.println(Arrays.toString(b));
 	}
 }
