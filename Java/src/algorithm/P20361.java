36aa2ebb75e1aee87f19706edbbce03704694553
Kim Hankil <nuclear0789@naver.com>
2021-07-26 23:20:21 +0900

    J20361

---
+++ Java/src/algorithm/P20361.java
@@ -0,0 +1,27 @@
+package algorithm;
+import java.io.*;
+import java.util.StringTokenizer;
+public class P20361 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(stk.nextToken());
+		int X = Integer.parseInt(stk.nextToken()); // ball is in X
+		int K = Integer.parseInt(stk.nextToken()); 
+		
+		int a, b;
+		for (int i = 0; i < K; i++) {
+			StringTokenizer steps = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(steps.nextToken());
+			b = Integer.parseInt(steps.nextToken());
+			if (X != a && X != b) {
+				continue;
+			}else if (X == a) {
+				X = b;
+			}else if (X == b) {
+				X = a;
+			}
+		}
+		System.out.println(X);
+	}
+}
