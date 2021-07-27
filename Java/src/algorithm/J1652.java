3fb434688c3b2fbf5f8aaf5997511a067e1ca357
Kim Hankil <nuclear0789@naver.com>
2021-07-28 02:15:28 +0900

    J1652 누울 자리를 찾아라 (문제 혼동 오지게함)

--- Java/src/algorithm/J1652.java
+++ Java/src/algorithm/J1652.java
@@ -19,9 +19,12 @@ public class J1652 {
 			overWall = true;
 			for (int j = 0; j < map.length-1; j++) {
 				if(map[i][j]=='X') {
+//					System.out.printf("%d%d 에서 true로 바꿨음\n",i,j);
 					overWall = true;
 				}
 				if(overWall && ((map[i][j]=='.') && (map[i][j+1]=='.'))) {
+//					System.out.printf("%b %b %b   ", overWall, map[i][j]=='.', map[i][j+1]=='.');
+//					System.out.printf("가로검사중%d%d %d%d는 연속..\n",i,j,i,j+1);
 					cAble++;
 					overWall = false;
 				}
@@ -32,9 +35,12 @@ public class J1652 {
 			overWall = true;
 			for (int j = 0; j < map.length-1; j++) {
 				if(map[j][i]=='X') {
+//					System.out.printf("%d%d 에서 true로 바꿨음\n",i,j);
 					overWall = true;
 				}
 				if(overWall && ((map[j][i]=='.') && (map[j+1][i]=='.'))) {
+//					System.out.printf("%b %b %b   ", overWall, map[j][i]=='.', map[j+1][i]=='.');
+//					System.out.printf("세로검사중%d%d %d%d는 연속..\n",j,i,j+1,i);
 					rAble++;
 					overWall = false;
 				}
