70b4f2cd8bb6a13706f0901d3152fc22f96dfe3e
Kim Hankil <nuclear0789@naver.com>
2021-08-03 02:14:17 +0900

    test for L1954 (최대 경우의 수 찾기위함)

--- Java/src/algorithm/Practice.java
+++ Java/src/algorithm/Practice.java
@@ -4,10 +4,16 @@ import java.util.Arrays;
 
 public class Practice {
 	public static void main(String[] args) {
-		int[] a = new int[] {1,2,3,4,5,6};
-		int[] b = new int[10];
-		System.out.println(Arrays.toString(b));
-		b = Arrays.copyOfRange(a, 2, 4);
-		System.out.println(Arrays.toString(b));
+		long n = 0;
+		long acc = 2*n*(n+1)*(2*n + 1);
+		double awer = Math.pow(10, 15);
+		System.out.println(awer);
+		while(acc < Math.pow(10, 15)) {
+			n++;
+			acc = 2*n*(n+1)*(2*n + 1);
+			System.out.printf("n = %d, acc = %d\n", n , acc);
+		}
+		
+		//10의 15승까지 가기위한 단계는 62996번
 	}
 }
