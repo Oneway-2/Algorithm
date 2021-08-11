229c47eae6976f5c87ed74ce4ea5fbc8eb41b085
Kim Hankil <nuclear0789@naver.com>
2021-08-12 03:11:01 +0900

    프로그래머스 H_Index - 정렬 - Level 2 - 문제를 읽고 정석적으로 표현한듯.

--- Java/src/programmers/sorting/H_Index.java
+++ Java/src/programmers/sorting/H_Index.java
@@ -1,5 +1,50 @@
 package programmers.sorting;
 
-public class H_Index {
+//import java.util.Arrays;
 
+public class H_Index {
+	public static void main(String[] args) {
+		Solution_HIndex sol = new Solution_HIndex();
+		
+//		int[] citations = new int[] {3,0,6,1,5};
+//		int[] citations = new int[] {88,89};
+//		int[] citations = new int[] {0,10,10,3,3,6,6,9,8,7,7};
+//		int[] citations = new int[] {0,0,0,0,1};
+//		int[] citations = new int[] {1,1,1,1,500,800};
+//		int[] citations = new int[] {1,2,3,500,800};
+//		int[] citations = new int[] {6,6,6,6,6,6};
+//		int[] citations = new int[] {0,1,2,3,4,5,6,7,8,9};
+//		int[] citations = new int[] {123,123,123,123,123,123,123,123,123};
+		int[] citations = new int[] {10,10,10,10,10,10};
+		
+		System.out.println(sol.solution(citations));
+	}
+	
+}
+
+class Solution_HIndex {
+	public int solution(int[] citations) {
+//		Arrays.sort(citations);
+		int answer = 0;
+		
+ex:		for (int i = citations.length; i >= 0; i--) {
+			int more = 0;
+			int less = 0;
+			
+			for (int j = 0; j < citations.length; j++) {
+				if (citations[j] >= i) {
+					more++;
+				}
+				else {
+					less++;
+				}
+				
+				if (more >= i && less <= i) {
+					answer = i;
+					break ex;
+				}
+			}
+		}
+		return answer;
+	}
 }
