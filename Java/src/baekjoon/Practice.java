2ae513eb3d345f588d50f7b52d860f50e5c418e5
Kim Hankil <nuclear0789@naver.com>
2021-09-22 23:06:13 +0900

    Practice

--- Java/src/baekjoon/Practice.java
+++ Java/src/baekjoon/Practice.java
@@ -1,76 +1,24 @@
 package baekjoon;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Practice {
-	static int N;
-	static int R;
-	static int[] numbers;
-	static int[] input;
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(stk.nextToken());
-		R = Integer.parseInt(stk.nextToken());
-		input = new int[N];
-		numbers = new int[R];
-
-		stk = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < input.length; i++) {
-			input[i] = Integer.parseInt(stk.nextToken());
+		String[] str = new String[100];
+		for (int i = 0; i < 100; i++) {
+			str[i] = String.valueOf(i);
 		}
 
-		Permu(0, 0);
-		System.out.println();
-		Combi(0, 0);
-		System.out.println();
-
-		dPermu(0);
-	}
-
-	private static void Combi(int cnt, int start) {
-		if (cnt == R) {
-			System.out.println(Arrays.toString(numbers));
-			return;
+		str[98] = "KBS2";
+		str[99] = "KBS1";
+		for (int i = 0; i < str.length; i++) {
+			System.out.println(str[i]);
 		}
-
-		for (int i = start; i < N; i++) {
-			numbers[cnt] = input[i];
-			Combi(cnt + 1, i);
-		}
-	}
-
-	private static void Permu(int cnt, int flag) {
-		if (cnt == R) {
-			System.out.println(Arrays.toString(numbers));
-			return;
-		}
-
-		for (int i = 0; i < N; i++) {
-			if ((flag & 1 << i) != 0) {
-				continue;
-			}
-			numbers[cnt] = input[i];
-			Permu(cnt + 1, flag | 1 << i);
-		}
-
-	}
-
-	private static void dPermu(int cnt) {
-		if (cnt == R) {
-			System.out.println(Arrays.toString(numbers));
-			return;
-		}
-		for (int i = 0; i < N; i++) {
-			numbers[cnt] = input[i];
-			dPermu(cnt + 1);
-
-		}
-
+		
+		System.out.println(("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111144444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444411111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111144444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444".length()));
+		
 	}
 }
