0df9191a08855f888fc8275502f7ad3af0e6cd8e
Kim Hankil <nuclear0789@naver.com>
2021-10-25 02:41:58 +0900

    백준 3048 개미 실버4 - 문자열, 시뮬, queue

--- Java/src/baekjoon/Main_백준_3048_개미_실버4_72ms.java
+++ Java/src/baekjoon/Main_백준_3048_개미_실버4_72ms.java
@@ -3,10 +3,11 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main_백준_3048_개미_실버4_ms {
+public class Main_백준_3048_개미_실버4_72ms {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stk = new StringTokenizer(br.readLine());
@@ -19,20 +20,34 @@ public class Main_백준_3048_개미_실버4_ms {
 		for (int i = 0, j = n1-1; i < n1; i++, j--) {
 			arr[i] = g1.charAt(j);
 		}
-		for (int i = n1, j = 0; i < n1+n2; i++, j++) {
-			arr[i] = g2.charAt(j);
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		for (int a = 0; a < sec; a++) {
+			for (int i = 1; i < arr.length; i++) {
+				if ('A' > arr[i] || arr[i] > 'Z') {
+					q.add(i++);
+				}
+			}
+			while(q.size() > 0) {
+				swap(q.poll(), arr);
+			}
 		}
 		
-		System.out.println(Arrays.toString(arr));
-		
-		for (int i = n1-1; i >= 0; i--) {
-			System.out.println(i);
-			swap(i, arr);
+		int idx = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if ('A' > arr[i] || arr[i] > 'Z') {
+				arr[i] = g2.charAt(idx++);
+			}
 		}
 		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
 	} // 메인 끝
-
 	private static void swap(int i, char[] arr) {
-		
+		char temp = arr[i-1];
+		arr[i-1] = arr[i];
+		arr[i] = temp;
 	}
 }
