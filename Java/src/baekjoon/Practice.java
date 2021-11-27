f5d4d59ebe4a187e042a7bb4437cbef351c3e192
Kim Hankil <nuclear0789@naver.com>
2021-11-27 23:54:00 +0900

    Queue contains 테스트

--- Java/src/baekjoon/Practice.java
+++ Java/src/baekjoon/Practice.java
@@ -5,26 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Practice {
 	public static void main(String[] args) {
-		int[][][] map = new int[3][2][5];
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(1);
+		q.offer(3);
+		q.offer(5);
+		q.offer(7);
+		q.offer(9);
 		
-		System.out.println(map.length);
-		System.out.println(map[0].length);
-		System.out.println(map[0][0].length);
-		
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				for (int k= 0; k < map[i][j].length; k++) {
-					System.out.print(map[i][j][k] + " ");
-				}
-				System.out.println();
-			}
-			System.out.println();
-		}
-		
+		System.out.println(q.contains(9));
+		System.out.println(q.contains(8));
+		System.out.println(q.contains(7));
+		System.out.println(q.contains(6));
 		
 	}
 	
