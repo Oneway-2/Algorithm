860aa91f726127549c6b79447944cc7f97b10bfe
Kim Hankil <nuclear0789@naver.com>
2021-12-22 00:29:47 +0900

    백준 11052 카드구매하기 실버1 - DP

--- Java/src/baekjoon/Main_백준_11052_카드구매하기_실버1_104ms.java
+++ Java/src/baekjoon/Main_백준_11052_카드구매하기_실버1_104ms.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_백준_11052_카드구매하기_실버1_ms {
+public class Main_백준_11052_카드구매하기_실버1_104ms {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
