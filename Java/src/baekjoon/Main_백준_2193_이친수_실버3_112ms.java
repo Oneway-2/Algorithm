ba650281e90a658d932ca9271bb38ee3e8e9bf5c
Kim Hankil <nuclear0789@naver.com>
2022-01-04 00:08:34 +0900

    백준 2193 이친수 실버3 - DP

---
+++ Java/src/baekjoon/Main_백준_2193_이친수_실버3_112ms.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main_백준_2193_이친수_실버3_112ms {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		long[] dp = new long[N+1];
+		if (N < 3) {
+			System.out.println(1);
+		}
+		else {
+			dp[1] = 1;
+			dp[2] = 1;
+			for (int i = 3; i <= N; i++) {
+				dp[i] = dp[i-1] + dp[i-2];
+			}
+			System.out.println(dp[N]);
+		}
+	} // 메인 끝
+}
