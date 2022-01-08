7cb84c2deabc8490a605d9dbade3849c59169a05
Kim Hankil <nuclear0789@naver.com>
2022-01-08 23:15:07 +0900

    문자열 연습

--- Java/src/baekjoon/Practice.java
+++ Java/src/baekjoon/Practice.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -11,20 +12,72 @@ import java.util.StringTokenizer;
 
 public class Practice {
 	public static void main(String[] args) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		q.offer(1);
-		q.offer(3);
-		q.offer(5);
-		q.offer(7);
-		q.offer(9);
+//		String hex = "1F7";
+//		int deci = 503;
+//		System.out.println(Integer.toHexString(deci));
+//		System.out.println(Integer.parseInt(hex, 16));
+//		
+//		System.out.println(hex.toUpperCase());
+//		
+//		String s = "1B3B3B81F75E";
+//		System.out.println(s);
+//		System.out.println(s.substring(1));
+//		System.out.print(s.substring(0,3) + " ");
+//		System.out.print(s.substring(3,6) + " ");
+//		System.out.print(s.substring(6,9) + " ");
+//		System.out.print(s.substring(9,12) + " ");
+////		System.out.println(s.concat(s.substring(0,1)));
+//		
+//		LinkedList<String> l = new LinkedList<String>();
+//		l.add("STR");
+//		System.out.println(l.contains("STR"));
+//		
+//		String data= "{\"result\":[{\"right\":\"37.22\",\"left\":\"127.02\"}]}";
+//		System.out.println(data);
+//		System.out.println(data.indexOf(":\""));
+//		
+//		System.out.println("arr test");
+//		ArrayList<Integer> aaa = new ArrayList<Integer>();
+//		aaa.add(64);
+//		aaa.add(128);
+//		System.out.println(aaa.toString());
+//		aaa.remove(aaa.size()-1);
+//		System.out.println(aaa.toString());
 		
-		System.out.println(q.contains(9));
-		System.out.println(q.contains(8));
-		System.out.println(q.contains(7));
-		System.out.println(q.contains(6));
+//		int[] numbers = new int[9];
+//		boolean[] selected = new boolean[9];
+//		numbers[3] = 1;
+//		selected[3] = true;
+//		System.out.println("go");
+//		perm(0, selected, numbers);
+		
+		String s = "ma";
+		String k = "sk";
+		String sk = s.concat(k);
+		System.out.println(sk);
+		String m ="turn";
+		
 		
 	}
-	
+
+//	private static void perm(int cnt, boolean[] selected, int[] numbers) {
+//		// TODO Auto-generated method stub
+//		if (cnt == 8) {
+//			System.out.println(Arrays.toString(numbers));
+//			return;
+//		}
+//		
+//		for (int i = 0; i < 9; i++) {
+//			if (selected[i]) {
+//				continue;
+//			}
+//			numbers[cnt] = i;
+//			selected[i] = true;
+//			perm(cnt+1, selected, numbers);
+//			selected[i] = false;
+//		}
+//		
+//	}
 
 		
 }
