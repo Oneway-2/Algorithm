e57c7c0425e80facc0b0e619e1af574a345e4834
김한길 <nuclear0789@naver.com>
2022-02-22 23:58:43 +0900

    백준 7510 고급수학 브론즈3 - 수학

---
+++ Java/src/baekjoon/Main_백준_7510_고급수학_브론즈3_ms.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_백준_7510_고급수학_브론즈3_ms {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk;
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[3];
+		for (int i = 1; i <= n; i++) {
+			stk = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				arr[j] = Integer.parseInt(stk.nextToken());
+			}
+			Arrays.sort(arr);
+			System.out.printf("Scenario #%d:\n", i);
+			if (arr[2]*arr[2] == (arr[0]*arr[0])+(arr[1]*arr[1])) {
+				System.out.println("yes");
+			}
+			else {
+				System.out.println("no");
+			}
+		}
+	} // 메인 끝
+}
