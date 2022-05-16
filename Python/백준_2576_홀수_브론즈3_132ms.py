aab7a2fbd7e8953c520e9edd21ae11956e661b86
김한길 <nuclear0789@naver.com>
2022-05-17 01:14:19 +0900

    백준_2576_홀수_브론즈3_132ms 구현

--- Python/백준_2576_홀수_브론즈3_132ms.py
+++ Python/백준_2576_홀수_브론즈3_132ms.py
@@ -1,12 +1,12 @@
 sum = 0
-min = 9999999999
+min = 999999
 for i in range(7):
     s = int(input())
     if s % 2 == 1:
         sum += s
         if s < min:
             min = s
-if s == 0:
+if sum == 0:
     print(-1)
 else:
     print(sum)
