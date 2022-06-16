1954ab874dce351cfe871a5ee62673fb4cb23bb7
김한길 <nuclear0789@naver.com>
2022-06-17 00:20:13 +0900

    백트랙킹, 순열 (시간초과)

--- Python/백준_1339_단어수학_골드4_ms.py
+++ Python/백준_1339_단어수학_골드4_ms.py
@@ -1,5 +1,10 @@
+# 시간 초과 뜸.
+# 답이 나오긴 하지만,, 순열을 쓰면 경우의 수가 너무 많다.
+
 import sys
 import itertools
+import time
+start = time.time()
 input = sys.stdin.readline
 N = int(input())
 alpa = [[] for i in range(N)]
@@ -11,23 +16,17 @@ for idx in range(N):
 
 alist = list(alist)
 one_ten = list(range(10))
-print(alist)
-
-nlist = map(list, itertools.combinations(one_ten, len(alist)))
+nlist = map(list, itertools.permutations(one_ten, len(alist)))
 
 result = 0
 for i in nlist:
     sum = 0
-    print(i)
     for j in alpa:
         tmp = []
         for k in j:
-            # print(k, end=" ")
-            # tmp.append(k)
             tmp.append(str(i[alist.index(k)]))
         con = ''.join(tmp)
-        print("con = ", con)
         sum += int(con)
     result = max(result, sum)
-    print("result = ", result)
+print("걸린 시간 : ", time.time()-start)
 print(result)
