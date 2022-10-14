237081fac977ac8609afa6c46a71b401fb5adc4c
김한길 <nuclear0789@naver.com>
2022-10-14 23:24:38 +0900

    정규표현식

--- Python/백준_1543_문서검색_실버4_108ms.py
+++ Python/백준_1543_문서검색_실버4_108ms.py
@@ -1,18 +1,40 @@
+import re
 import sys
 input = sys.stdin.readline
 
-T = input()
+# ===== 3차 풀이 ===== 196ms
+T = input().rstrip()
 text = input().rstrip()
+regex = re.compile(text)
+cnt = 0
+while True:
+    m = regex.search(T)
+    if not m:
+        break
+    cnt += 1
+    T = T[m.end():]
+print(cnt)
 
-leng = len(text)
-idx = 0
-answer = 0
-while idx < len(T)-leng:
-    if T[idx:idx+leng] == text:
-        # print("idx = {} 맞다잉?".format(idx))
-        idx += leng
-        answer += 1
-    else:
-        idx += 1
 
-print(answer)
+# ===== 2차 풀이 ===== 116ms
+# T = input().rstrip()
+# text = input().rstrip()
+# print(T.count(text))
+
+
+# ===== 1차 풀이 ===== 108ms
+# T = input()
+# text = input().rstrip()
+
+# leng = len(text)
+# idx = 0
+# answer = 0
+# while idx < len(T)-leng:
+#     if T[idx:idx+leng] == text:
+#         # print("idx = {} 맞다잉?".format(idx))
+#         idx += leng
+#         answer += 1
+#     else:
+#         idx += 1
+
+# print(answer)
