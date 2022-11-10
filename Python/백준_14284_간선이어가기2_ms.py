dee40d20cc6cff88b4d44ffa56ece02722b9b34a
김한길 <nuclear0789@naver.com>
2022-11-11 01:03:23 +0900

    다익스트라, 그래프

--- Python/백준_14284_간선이어가기2_ms.py
+++ Python/백준_14284_간선이어가기2_ms.py
@@ -5,23 +5,29 @@ input = sys.stdin.readline
 N, M = map(int, input().split(" "))
 graph = [[] for i in range(N+1)]
 INF = sys.maxsize
-distance = [[INF for i in range(N+1)] for i in range(N+1)]
+distance = [INF for i in range(N+1)]
 for i in range(M):
     a, b, val = map(int, input().split(" "))
     graph[a].append((b, val))
     graph[b].append((a, val))
 finalA, finalB = map(int, input().split(" "))
+distance[finalA] = 0
 
 heap = []
 for i in graph[finalA]:
     to, val = i[0], i[1]
     heapq.heappush(heap, (to, val))
-    distance[finalA][to] = val
+    distance[to] = val
 
-    while heap:
-        fr, frval = heapq.heappop(heap)
-        # print("fr = {}, frval = {}".format(fr, frval))
-        for j in graph[fr]:
-            frto, frtoval = j[0], j[1]
-            print("frto = {}, frtoval = {}".format(frto, frtoval))
-            heapq.heappush()
+while heap:
+    fr, val = heapq.heappop(heap)
+    for j in graph[fr]:
+        to, toval = j[0], j[1]
+        newVal = val + toval
+        if distance[to] > newVal:
+            distance[to] = newVal
+            heapq.heappush(heap, (to, newVal))
+
+print(distance[finalB])
+
+# 다익스트라
